c20b2acc996f6edec25fee2da6de4a3d05efb235
S00180333 <36006393+S00180333@users.noreply.github.com>
2020-12-28 03:24:37 +0000

    QRCode

---
+++ app/src/main/java/com/gavankennedy/qrcode/MainActivity.java
@@ -0,0 +1,58 @@
+package com.gavankennedy.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etName, etAddress;
+    private IntentIntegrator qrscan = new IntentIntegrator(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etName = findViewById(R.id.etName);
+        etAddress = findViewById(R.id.etAddress);
+    }
+
+    public void doScan(View view) {
+        qrscan.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode,
+                data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+
+                    JSONObject obj = new JSONObject(result.getContents());
+                    etName.setText(obj.getString("title"));
+                    etAddress.setText(obj.getString("website"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
